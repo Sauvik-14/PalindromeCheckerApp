cc854f724b32e9b580284ed0915a7b2e2fcca858
Sauvik-14 <zenx.official5914@gmail.com>
2026-02-16 11:05:10 +0530

    UC1:Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+/*
+*@author Sauvik
+* Version 1.0
+*/
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome To Palindrome Checker Management App");
+        System.out.println("Version: 1.0");
+        System.out.println("System initiated successfully.");
+    }
+}
