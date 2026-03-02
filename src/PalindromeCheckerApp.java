d509a10afe5b0d79be0995de4cc12f28e0579670
Sauvik-14 <zenx.official5914@gmail.com>
2026-03-02 03:19:24 +0000

    UC10 - Case-Insensitive & Space-Ignored Palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,30 +1,35 @@
-public class UseCase9PalindromeCheckerApp {
+public class UseCase10PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version: 1.0");
 
-        String word = "madam";
+        String word = "Madam In Eden Im Adam";
 
-        if (isPalindrome(word, 0, word.length() - 1)) {
-            System.out.println(word + " is a Palindrome.");
+        // Normalize string (remove spaces and convert to lowercase)
+        String normalized = word.replaceAll("\\s+", "").toLowerCase();
+
+        boolean isPalindrome = true;
+
+        int start = 0;
+        int end = normalized.length() - 1;
+
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("\"" + word + "\" is a Palindrome.");
         } else {
-            System.out.println(word + " is NOT a Palindrome.");
+            System.out.println("\"" + word + "\" is NOT a Palindrome.");
         }
 
         System.out.println("System execution completed.");
-    }
-
-    // Recursive function
-    public static boolean isPalindrome(String str, int start, int end) {
-
-        if (start >= end)
-            return true;
-
-        if (str.charAt(start) != str.charAt(end))
-            return false;
-
-        return isPalindrome(str, start + 1, end - 1);
     }
 }
