a134cf540f6ed961cc0c633fa58e3c98befdc16e
Sauvik-14 <zenx.official5914@gmail.com>
2026-03-02 03:02:46 +0000

    UC2 - Print a Hardcoded Palindrome Result

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,11 +1,20 @@
-/*
-*@author Sauvik
-* Version 1.0
-*/
-public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        System.out.println("Welcome To Palindrome Checker Management App");
+public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version: 1.0");
-        System.out.println("System initiated successfully.");
+
+        // Hardcoded string
+        String word = "madam";
+
+        // Palindrome check
+        if (word.equals("madam")) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
+
+        System.out.println("System execution completed.");
     }
 }
