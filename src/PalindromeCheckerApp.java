c03f1878fde0113bfcc2f5381d8fb125b83e55a5
Sauvik-14 <zenx.official5914@gmail.com>
2026-03-02 03:35:28 +0000

    UC1 - Application Entry & Welcome Message

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,35 +1,11 @@
-public class UseCase10PalindromeCheckerApp {
-
-    public static void main(String[] args) {
-
-        System.out.println("Welcome to the Palindrome Checker Management System");
+/*
+*@author Sauvik
+* Version 1.0
+*/
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome To Palindrome Checker Management App");
         System.out.println("Version: 1.0");
-
-        String word = "Madam In Eden Im Adam";
-
-        // Normalize string (remove spaces and convert to lowercase)
-        String normalized = word.replaceAll("\\s+", "").toLowerCase();
-
-        boolean isPalindrome = true;
-
-        int start = 0;
-        int end = normalized.length() - 1;
-
-        while (start < end) {
-            if (normalized.charAt(start) != normalized.charAt(end)) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
-        }
-
-        if (isPalindrome) {
-            System.out.println("\"" + word + "\" is a Palindrome.");
-        } else {
-            System.out.println("\"" + word + "\" is NOT a Palindrome.");
-        }
-
-        System.out.println("System execution completed.");
+        System.out.println("System initiated successfully.");
     }
 }
